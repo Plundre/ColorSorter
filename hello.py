b7cdee6e8ff6a6eff5a7b48cc80bbab751a7d9b9
Plundre <eivind.l@hotmail.com>
2020-12-02 21:27:29 +0100

    Nesten funker, Sensor kan spise kuk

--- hello.py
+++ hello.py
@@ -5,12 +5,11 @@ import os
 import sys
 import time
 from array import *
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D, SpeedPercent, MoveTank
 
 from ev3dev2.sensor import INPUT_1, INPUT_2
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor
 from ev3dev2.led import Leds
-from ev3dev2.motor import 
 #Errors/Return Constants
 ERROR_NO_BLOCK_ON_BELT = 69
 
@@ -18,8 +17,20 @@ ERROR_NO_BLOCK_ON_BELT = 69
 STRG_CTRL_THRESHOLD = 5
 FEEDBELT_ONE_BLOCK_TO_DEG = 92
 
-BAR_STATE_READY_TO_RETRACT = 180
-BAR_STATE_READY_TO_EXTEND = 720
+BAR_STATE_READY_TO_RETRACT = -1#80
+BAR_STATE_READY_TO_EXTEND = -7#20
+
+legalColor1 = 2
+legalColor2 = 4
+legalColor3 = 5
+
+POS_CHAMBER1 = (170*6)
+POS_CHAMBER2 = (170*5)
+POS_CHAMBER3 = (170*4)
+POS_CHAMBER4 = (170*3)
+POS_CHAMBER5 = (170*2)
+POS_CHAMBER6 = 170
+POS_TRASH = 0
 
 # state constants
 ON = True
@@ -51,9 +62,9 @@ def set_font(name):
 
 class StorageUnit():
     storage = array('i', [0,0,0,0,0,0])
-
+    countyBoi = 0
     trashCounter = 0
-
+    color = 4
     def addBlock(self, blockType):
 
         """
@@ -61,44 +72,152 @@ class StorageUnit():
         """
         i = 0
         while(i < len(self.storage)):
-            if (i == blockType and i < 3):
+            if (i == (blockType-1) and i < 3):
                 self.storage[i] += 1
-            elif(i== blockType and i > 2):
+            elif(i== (blockType-1) and i > 2):
                 self.storage[i] += 2
             i+=1
 
     def countTrash(self):
         self.trashCounter += 1
 
-    def RunStorageUnit(deg):
+    def RunStorageUnit(self,deg):
         storageMotor = LargeMotor(OUTPUT_A)
         storageMotor.on_for_degrees(speed=SPEED, degrees=deg)
 
-    def GetPos_StorageUnit():
-        storageEncoder = LargeMotor
+    def GetPos_StorageUnit(self):
+        storageEncoder = LargeMotor(OUTPUT_A).position
+        return storageEncoder
+
+    def Sorting_A_Block(self, legalColor1, legalColor2, legalColor3):
+        feedBelt = FeedBelt()
+        pushBar = PushBar()
+        sensors = SensorArray()
+        #print('Hyll Satan')
+        blockUnderSorting = True
+        
+        #If colorSensor doesnt see anything, run FeedBelt until it sees something or it has run 2 Turns
+        
+        if(sensors.ReadColor(legalColor1,legalColor2,legalColor3) > 0):
+        
+            while(blockUnderSorting == True):
+            
+                self.color = sensors.ReadColor(legalColor1, legalColor2, legalColor3)
+                        
+                #setVal = Color_To_SetVal(color, legalColor1, legalColor2, legalColor3)
+                #print('Hail Cthulu')
+                if(self.color > 0 and self.color < 4):
+                    pushBar.RunBar(BAR_STATE_READY_TO_RETRACT)
+                    print('Hail Asmoduus')
+                    feedBelt.RunBelt(FEEDBELT_ONE_BLOCK_TO_DEG)
+                    time.sleep(0.5)
+
+                    setValue = self.color + sensors.ReadLength(legal1=legalColor1, legal2=legalColor2, legal3=legalColor3)
+                    time.sleep(0.5)
+
+                    self.StorageControl(setValue)
+
+                    pushBar.RunBar(0-BAR_STATE_READY_TO_RETRACT)
+                    time.sleep(0.5)
+
+                    pushBar.RunBar(BAR_STATE_READY_TO_RETRACT + BAR_STATE_READY_TO_EXTEND - ( (self.storage[setValue-1] + 1) * 102))
+
+                    pushBar.RunBar(0 - (BAR_STATE_READY_TO_RETRACT + BAR_STATE_READY_TO_EXTEND - ((self.storage[setValue-1]+1)*102) ) )
+
+                    self.addBlock(setValue)
+
+                    blockUnderSorting = False
+            
+                elif(self.color == 4):
+                    pushBar.RunBar(BAR_STATE_READY_TO_RETRACT)
+                    feedBelt.RunBelt(FEEDBELT_ONE_BLOCK_TO_DEG)
+                    time.sleep(0.5)
+                    #print('Beelzebub')
+                    print(self.countyBoi)
+
+                    self.countyBoi = self.countyBoi + 1
+                    #if(countyBoi == 2 or countyBoi == 4 or countyBoi == 6 or countyBoi == 8 or countyBoi == 10 or countyBoi == 12):
+                    #    print('Fytti Katta og hutti tu')
+                    #    print(countyBoi)
+                    setValue = 7 #Dvs utenfor array-et
+                    time.sleep(0.5)
+
+                    self.StorageControl(setValue)
+
+                    pushBar.RunBar(0-BAR_STATE_READY_TO_RETRACT)
+                    time.sleep(0.5)
+
+                    pushBar.RunBar(BAR_STATE_READY_TO_RETRACT + BAR_STATE_READY_TO_EXTEND)
+
+                    pushBar.RunBar(0 - BAR_STATE_READY_TO_EXTEND)
+
+                    self.countTrash()
+
+                    blockUnderSorting = False
+                else:
+                    blockUnderSorting = False
+                    #If end up here, something went wrong (Might not need elif, just use else instead)                        
+        else:
+            #What To do when there is no block seen
+            
+            return ERROR_NO_BLOCK_ON_BELT
+
+    def SortingProtocoll(self):
+        numOfBlocks = 12
+        while(numOfBlocks > 0):
+            
+            self.Sorting_A_Block(legalColor1,legalColor2,legalColor3)
+            #print('naere no')
+            numOfBlocks= numOfBlocks - 1
+    
+    def SetVal_To_SetDeg(self,setVal):
+        if(setVal == 1):    
+            return POS_CHAMBER1
+        elif(setVal == 2):
+            return POS_CHAMBER2
+        elif(setVal == 3):
+            return POS_CHAMBER3
+        elif(setVal == 4):
+            return POS_CHAMBER4
+        elif(setVal == 5):
+            return POS_CHAMBER5
+        elif(setVal == 6):
+            return POS_CHAMBER6
+        elif(setVal == 7):
+            return POS_TRASH            
+    
+    def StorageControl(self,setValue):
+        setDeg = self.SetVal_To_SetDeg(setVal=setValue)
+        if((setDeg - self.GetPos_StorageUnit()) > STRG_CTRL_THRESHOLD):
+            self.RunStorageUnit(2)
+        elif(self.GetPos_StorageUnit() - setDeg > STRG_CTRL_THRESHOLD):
+            self.RunStorageUnit(-2)
+        else:
+            self.RunStorageUnit(0)
 
 
 class FeedBelt():
-    def RunBelt(deg):
+    def RunBelt(self, deg):
         beltMotor = LargeMotor(OUTPUT_B)
         beltMotor.on_for_degrees(speed=SPEED, degrees=deg)
+        
     
-    def GetPos_Belt():
+    def GetPos_Belt(self):
         beltEncoder = LargeMotor(OUTPUT_B)
         return beltEncoder.position
 
 class PushBar():
-    def RunBar(deg):
+    def RunBar(self, deg):
         barMotor = LargeMotor(OUTPUT_D)
-        barMotor.on_for_degrees(deg)
+        barMotor.on_for_degrees(degrees=deg, speed=SPEED)
 
-    def GetPos_Bar():
+    def GetPos_Bar(self):
         barEncoder = LargeMotor(OUTPUT_D)
         return barEncoder.position
 
 class SensorArray():
 
-    def ReadColor(legal1, legal2, legal3):
+    def ReadColor(self, legal1, legal2, legal3):
         colorSens = ColorSensor(INPUT_1)
         if(colorSens == legal1):
             return 1
@@ -112,7 +231,7 @@ class SensorArray():
             return 4
         #return  colorSens.color
 
-    def ReadLength(legal1, legal2, legal3):
+    def ReadLength(self, legal1, legal2, legal3):
         LengthSens = ColorSensor(INPUT_2)
         readVal = LengthSens.color
         if(readVal == legal1 or readVal == legal2 or readVal == legal3):
@@ -120,99 +239,22 @@ class SensorArray():
         else:
             return 0
 
-def StorageControlSystem(setVal):
-    storage = StorageUnit()
+    #def Color_To_SetVal(color, legal1, legal2, legal3):
+    #    if(color == legal1):
+    #        return 1
+    #    elif (color == legal2):
+    #        return 2
+    #    elif (color == legal3):
+    #        return 3
+    #    else:
+    #        return 0
 
+    #Run Belt
+    #Read Length
+    #Run Feed-Bar
 
-#def Color_To_SetVal(color, legal1, legal2, legal3):
-#    if(color == legal1):
-#        return 1
-#    elif (color == legal2):
-#        return 2
-#    elif (color == legal3):
-#        return 3
-#    else:
-#        return 0
-        
-
-
-
-def SortingProtocoll(legalColor1, legalColor2, legalColor3):
-    storage = StorageUnit()
-    feedBelt = FeedBelt()
-    pushBar = PushBar()
-    sensors = SensorArray()
-    
-    blockUnderSorting = True
-    #If colorSensor doesnt see anything, run FeedBelt until it sees something or it has run 2 Turns
-    
-    
-    if(sensors.ReadColor > 0)
-        
-        while(blockUnderSorting == True)
-            
-            color = sensors.ReadColor(legalColor1, legalColor2, legalColor3)
-            
-            
-            #setVal = Color_To_SetVal(color, legalColor1, legalColor2, legalColor3)
-            
-            
-            
-            if(color > 0 and color < 4):
-                feedBelt.RunBelt(FEEDBELT_ONE_BLOCK_TO_DEG)
-                time.sleep(0.5)
-                
-                setValue = color + sensors.ReadLength(legalColor1, legalColor2, legalColor3)
-                time.sleep(0.5)
-
-                StorageControlSystem(setValue)
-
-                pushBar.RunBar(0-BAR_STATE_READY_TO_RETRACT)
-                time.sleep(0.5)
-
-                pushBar.RunBar(BAR_STATE_READY_TO_RETRACT + BAR_STATE_READY_TO_EXTEND - ( (storage.storage[setValue] + 1) * 102))
-                
-                pushBar.RunBar(0 - ( BAR_STATE_READY_TO_EXTEND - ((storage.storage[setValue]+1)*102) ) )
-
-                storage.addBlock(setValue)
-            
-            elif(color == 4)
-                feedBelt.RunBelt(FEEDBELT_ONE_BLOCK_TO_DEG)
-                time.sleep(0.5)
-                
-                setValue = 7 #Dvs utenfor array-et
-                time.sleep(0.5)
-
-                StorageControlSystem(setValue)
-
-                pushBar.RunBar(0-BAR_STATE_READY_TO_RETRACT)
-                time.sleep(0.5)
-
-                pushBar.RunBar(BAR_STATE_READY_TO_RETRACT + BAR_STATE_READY_TO_EXTEND)
-                
-                pushBar.RunBar(0 - BAR_STATE_READY_TO_EXTEND)
-
-                storage.countTrash()
-            else:
-                #If end up here, something went wrong (Might not need elif, just use else instead)                        
-    else:
-        #What To do when there is no block seen
-        return ERROR_NO_BLOCK_ON_BELT
-    
-    
-
-
-
-
-
-
-
-#Run Belt
-#Read Length
-#Run Feed-Bar
-
-
-def OrderBlock():
+def OrderBlock():   
+    return 0    
 
 
 def HMI():
@@ -222,19 +264,20 @@ def HMI():
 
     return mode
 
-def 
+
 
 def main():
     running = True
-    modeSelect = 0
+    modeSelect = 1
     storage = StorageUnit()
-
+    print('whaat')
     while running:
-        while(modeSelect = 0):
+        while(modeSelect == 1):
             modeSelect = HMI()
-            if (modeSelect = 1):
-                SortBlock()
-            elif (modeSelect = 2):
+            if (modeSelect == 1):
+                print('Alyoo')
+                storage.SortingProtocoll()
+            elif (modeSelect == 2):
                 OrderBlock()
         
 
